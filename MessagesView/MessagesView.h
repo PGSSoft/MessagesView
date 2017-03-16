197828144dff815cb253bc3079991ef02c58531f
Damian Kanak <dkanak@pgs-soft.com>
2017-03-16 15:58:15 +0100

    Initial Commit

---
+++ MessagesView/MessagesView.h
@@ -0,0 +1,19 @@
+//
+//  MessagesView.h
+//  MessagesView
+//
+//  Created by pgs-dkanak on 16/03/17.
+//  Copyright © 2017 pgs-dkanak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MessagesView.
+FOUNDATION_EXPORT double MessagesViewVersionNumber;
+
+//! Project version string for MessagesView.
+FOUNDATION_EXPORT const unsigned char MessagesViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessagesView/PublicHeader.h>
+
+
